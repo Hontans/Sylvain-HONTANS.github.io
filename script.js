fb71a85f1bc3f96406f59dbc948d07738038fc3f
Hontans <163842666+Hontans@users.noreply.github.com>
2025-01-04 17:28:01 +0000

    Refactor JavaScript for random text generation and improve rain drop creation logic

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const cloud = document.querySelector('.cloud');
 
 const randomText = () => {
-    const text = '☁️';
-    return text[Matj.floor(Math.random() * text.length)];
-    }
+    const text = ("!@#$%^*()")
+    return text[Math.floor(Math.random() * text.length)];
+};
 
 const rain = () => {
     const e = document.createElement('div');
     e.classList.add('drop');
     cloud.appendChild(e);
 
-    const left = Math.floor(Math.random()* 300)
+    const left = Math.floor(Math.random() * 300)
     const size = Math.random() * 1.5;
     const duration = Math.random() * 1;
 
@@ -19,7 +19,11 @@ const rain = () => {
     e.style.fontSize = `${0.5 + size}em`;
     e.style.animationDuration = `${1 + duration}s`;
 
-    setTimeout(() => cloud.removeChild(e), 2000);
+    setTimeout(
+        () => cloud.removeChild(e), 2000,
+    );
 };
 
-setInterval(() => rain(), 20,);
+setInterval(
+    () => rain(), 20,
+);
