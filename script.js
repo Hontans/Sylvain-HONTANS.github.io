60a4b7b271145e65b59af2b394f75884067289af
Hontans <163842666+Hontans@users.noreply.github.com>
2025-01-04 16:32:29 +0000

    Add JavaScript functionality for cloud animation and raindrop effect

---
+++ script.js
@@ -0,0 +1,25 @@
+const cloud = document.querySelector('.cloud');
+
+const randomText = () => {
+    const text = '☁️';
+    return text[Matj.floor(Math.random() * text.length)];
+    }
+
+const rain = () => {
+    const e = document.createElement('div');
+    e.classList.add('drop');
+    cloud.appendChild(e);
+
+    const left = Math.floor(Math.random()* 300)
+    const size = Math.random() * 1.5;
+    const duration = Math.random() * 1;
+
+    e.innerText = randomText();
+    e.style.left = `${left}px`;
+    e.style.fontSize = `${0.5 + size}em`;
+    e.style.animationDuration = `${1 + duration}s`;
+
+    setTimeout(() => cloud.removeChild(e), 2000);
+};
+
+setInterval(() => rain(), 20,);
