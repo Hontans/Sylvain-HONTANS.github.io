46295c06ac493010fc92c46f4f3cfab76859dc38
Hontans <163842666+Hontans@users.noreply.github.com>
2025-01-04 18:02:30 +0000

    Wrap rain and randomText functions in DOMContentLoaded event for improved execution timing

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const cloud = document.querySelector('.cloud');
+document.addEventListener('DOMContentLoaded', () => {
+    const cloud = document.querySelector('.cloud');
 
-const randomText = () => {
-    const text = ("!@#$%^*()")
-    return text[Math.floor(Math.random() * text.length)];
-};
+    const randomText = () => {
+        const text = ("!@#$%^*()")
+        return text[Math.floor(Math.random() * text.length)];
+    };
 
-const rain = () => {
-    const e = document.createElement('div');
-    e.classList.add('drop');
-    cloud.appendChild(e);
+    const rain = () => {
+        const e = document.createElement('div');
+        e.classList.add('drop');
+        cloud.appendChild(e);
 
-    const left = Math.floor(Math.random() * 300)
-    const size = Math.random() * 1.5;
-    const duration = Math.random() * 1;
+        const left = Math.floor(Math.random() * 300)
+        const size = Math.random() * 1.5;
+        const duration = Math.random() * 1;
 
-    e.innerText = randomText();
-    e.style.left = `${left}px`;
-    e.style.fontSize = `${0.5 + size}em`;
-    e.style.animationDuration = `${1 + duration}s`;
+        e.innerText = randomText();
+        e.style.left = `${left}px`;
+        e.style.fontSize = `${0.5 + size}em`;
+        e.style.animationDuration = `${1 + duration}s`;
 
-    setTimeout(
-        () => cloud.removeChild(e),
-        2000,
+        setTimeout(
+            () => cloud.removeChild(e),
+            2000,
+        );
+    };
+
+    setInterval(
+        () => rain(),
+        20,
     );
-};
-
-setInterval(
-    () => rain(),
-    20,
-);
+});
